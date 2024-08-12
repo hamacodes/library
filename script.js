062be825cdef85b64d632a57dcf6eb43ff5f8021
Hama <pashazad@msu.edu>
2024-08-12 19:38:24 -0400

    chore: Refactor form button and book card layout

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 
 
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book() {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
   this.info = function() {
     return `${this.title} by ${this.author}, has ${pages} pages, read = ${this.read}`
   }
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  return myLibrary;
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "no");
