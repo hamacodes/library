b28ab6691c30a0b253d2c4bc491ab1b492c9b050
Hama <pashazad@msu.edu>
2024-08-12 17:12:25 -0400

    feat: Add Book constructor and sample books to library

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+const myLibrary = [];
+
+function Book() {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return `${this.title} by ${this.author}, has ${pages} pages, read = ${this.read}`
+  }
+}
+
+function addBookToLibrary() {
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "no");
+const savageSon = new Book("Savage Son", "Jack Carr", 400, "yes");
+const onlyTheDead = new Book("Only The Dead", "Jack Carr", 350, "yes");
+const redSkyMourning = new Book("Red Sky Mourning", "Jack Carr", 450, "yes");
