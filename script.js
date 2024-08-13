5680fa0e6ba22ab70f25c4206c802ec0391e8902
Hama <pashazad@msu.edu>
2024-08-12 20:02:48 -0400

    Refactor book card layout and appearance

--- script.js
+++ script.js
@@ -11,8 +11,29 @@ class Book {
   }
 }
 
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "no");
+const savageSon = new Book("Savage Son", "Jack Carr", 400, "yes");
+const onlyTheDead = new Book("Only The Dead", "Jack Carr", 350, "yes");
+const redSkyMourning = new Book("Red Sky Mourning", "Jack Carr", 450, "yes");
+
+const myLibrary = [theHobbit,savageSon,onlyTheDead,redSkyMourning];
+
 function createBookCard(book) {
-  
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("book-card");
+  bookCard.id = myLibrary.indexOf(book);
+  bookCard.innerHTML = 
+  `
+    <h2>${book.title}</h2>
+    <p>By: ${book.author}</p>
+    <p>Pages: ${book.pages}</p>
+    <p>Read: ${book.read}</p>
+  `;
+  return bookCard;
+}
+myLibrary.forEach(book => {
+  console.log(book.info());
+});
 
 function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
@@ -28,16 +49,4 @@ addBookButton.addEventListener("click", () => {
   let read = document.querySelector("#read").value;
   addBookToLibrary(title, author, pages, read);
   alert(myLibrary);
-});
-
-
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "no");
-const savageSon = new Book("Savage Son", "Jack Carr", 400, "yes");
-const onlyTheDead = new Book("Only The Dead", "Jack Carr", 350, "yes");
-const redSkyMourning = new Book("Red Sky Mourning", "Jack Carr", 450, "yes");
-
-const myLibrary = [theHobbit,savageSon,onlyTheDead,redSkyMourning];
-
-myLibrary.forEach(book => {
-  console.log(book.info());
 });
